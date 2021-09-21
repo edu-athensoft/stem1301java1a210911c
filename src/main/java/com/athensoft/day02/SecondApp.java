f9948d09c8133623ac70dc1641e5a8419dd23834
athensedu <lada314@gmail.com>
2021-09-21 17:09:59 -0400

    added day02

---
+++ src/main/java/com/athensoft/day02/SecondApp.java
@@ -0,0 +1,12 @@
+package com.athensoft.day02;
+
+// SecondApp2 is not correct
+
+public class SecondApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
