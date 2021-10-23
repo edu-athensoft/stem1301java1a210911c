d42c16ea80da77050eaec3537a16ca0e0550d90f
athensedu <lada314@gmail.com>
2021-10-23 12:48:07 -0400

    added quiz4 and quiz5

---
+++ src/main/java/com/athensoft/day06/Quiz4.java
@@ -0,0 +1,25 @@
+package com.athensoft.day06;
+
+/**
+ * @author Athens
+ *
+ */
+public class Quiz4 {
+	public static void main(String[] args) {
+//		a = 0;
+//		int a = 0;
+//		
+//		String a = "my string";
+		
+//		int b = 5;
+//		double b = 6;
+		
+//		int myNumber = 200147483648;
+		long myNumber = 200147483648L;
+		
+		long num2 = 10;
+		
+		
+	
+	}
+}
---
+++ src/main/java/com/athensoft/day06/Quiz5.java
@@ -0,0 +1,14 @@
+package com.athensoft.day06;
+
+public class Quiz5 {
+
+	public static void main(String[] args) {
+		boolean b1 = true;
+		
+//		boolean b2 = True;
+		
+		
+
+	}
+
+}
